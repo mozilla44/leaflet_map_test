a75446ab57287f3f3bbad91074559f6cd67b8023
Antoine <antoine.frisco@gmail.com>
2024-06-18 18:02:30 +0200

    typo

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <>
-      <h1>Example de carte interactive 🗺️</h1>
+      <h1>Exemple de carte interactive 🗺️</h1>
       <h2>Cette carte codée par votre dev préféré est connectée à un tableau Airtable</h2>
       <p>Pour ajouter des coordonées, il suffit d'ajouter une entrée dans le tableau </p>
       <Map />
