a593989befa8192945da14e805db258751c6c57d
Antoine <antoine.frisco@gmail.com>
2024-06-18 21:21:48 +0200

    link

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const App: React.FC = () => {
     <div className="App">
       <>
       <h1>Exemple de carte interactive 🗺️</h1>
-      <h2>Cette carte codée par votre dev préféré est connectée à un tableau Airtable</h2>
+      <h2>Cette carte codée par votre dev préféré est connectée à <a href="https://airtable.com/app3loNcvd921Yy1G/shr2YGGa66G7iQfhB">un tableau Airtable</a></h2>
       <p>Pour ajouter des coordonées, il suffit d'ajouter une entrée dans le tableau </p>
       <Map />
       </>
