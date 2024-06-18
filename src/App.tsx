8d48c1f980be1b9c6179adfd11309868898ec926
Antoine <antoine.frisco@gmail.com>
2024-06-18 17:59:30 +0200

    final demo

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,12 @@ import Map from './Map';
 const App: React.FC = () => {
   return (
     <div className="App">
+      <>
+      <h1>Example de carte interactive 🗺️</h1>
+      <h2>Cette carte codée par votre dev préféré est connectée à un tableau Airtable</h2>
+      <p>Pour ajouter des coordonées, il suffit d'ajouter une entrée dans le tableau </p>
       <Map />
+      </>
     </div>
   );
 }
